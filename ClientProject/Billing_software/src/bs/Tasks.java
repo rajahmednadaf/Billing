68bc482501d853cb190ffe34528b2bef53098bac
rajahmednadaf <rajahmednadaf3@gmail.com>
2019-04-05 14:32:31 +0530

    login

---
+++ ClientProject/Billing_software/src/bs/Tasks.java
@@ -0,0 +1,47 @@
+package bs;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class Tasks extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tasks frame = new Tasks();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Tasks() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnAddProduct = new JButton("ADD PRODUCT");
+		btnAddProduct.setBounds(352, 31, 269, 44);
+		contentPane.add(btnAddProduct);
+	}
+
+}
